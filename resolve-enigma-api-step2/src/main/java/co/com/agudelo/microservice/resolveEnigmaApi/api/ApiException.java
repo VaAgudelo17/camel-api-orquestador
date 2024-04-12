d9e56e23f4a97d2c712616b2e5c730b02ece9702
VaAgudelo17 <114451968+VaAgudelo17@users.noreply.github.com>
2024-04-12 18:43:00 -0500

    Paso 2 (Put the girafe)

---
+++ resolve-enigma-api-step2/src/main/java/co/com/agudelo/microservice/resolveEnigmaApi/api/ApiException.java
@@ -0,0 +1,10 @@
+package co.com.agudelo.microservice.resolveEnigmaApi.api;
+
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2024-03-10T15:58:32.539-05:00[America/Bogota]")
+public class ApiException extends Exception{
+    private int code;
+    public ApiException (int code, String msg) {
+        super(msg);
+        this.code = code;
+    }
+}
